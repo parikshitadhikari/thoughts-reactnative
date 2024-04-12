6cfbda9535827e0d5dfb2e20a9a026b465a0cc31
parikshitadhikari <parikshitadhikari@gmail.com>
2024-04-12 12:57:50 +0545

    listing thoughts

--- src/components/ThoughtList.jsx
+++ src/components/ThoughtList.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
 
-const ThoughtList = () => {
+const ThoughtList = ({ thought, userInfo }) => {
+  // <Text>{userInfo.name}</Text>
   return (
-    <div>ThoughtList</div>
-  )
-}
-
-export default ThoughtList
+    <View style={styles.container}>
+      <Text>{thought.text}</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#c7bfbf",
+    marginBottom: 10,
+    padding: 15,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: 0.5,
+    shadowOpacity: 0.5,
+  },
+});
+export default ThoughtList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,30 +1,50 @@
 import React, { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { loadCredentials } from "../slices/authSlice";
+import { useGetAllThoughtQuery } from "../slices/thoughtApiSlice";
+import ThoughtList from "../components/ThoughtList";
 
-const HomePage = ({ navigate }) => {
-  const dispatch = useDispatch()
+const HomePage = () => {
+  const dispatch = useDispatch();
   const userInfo = useSelector((state) => state.auth.userInfo);
 
+  const { data, isFetching, isError } = useGetAllThoughtQuery();
+
   useEffect(() => {
-    dispatch(loadCredentials())
-  }, [dispatch])
+    dispatch(loadCredentials());
+  }, [dispatch]);
+
+  let content;
+  if (isFetching) {
+    content = <Text>Loading...</Text>;
+  } else if (!data || data.length === 0) {
+    content = <Text>No thoughts available</Text>;
+  } else if (isError) {
+    content = <Text>Error loading thoughts</Text>;
+  } else {
+    content = data.map((thought) => {
+      return (
+        <ThoughtList key={thought._id} thought={thought} userInfo={userInfo} />
+      );
+    });
+  }
 
   return (
-    <View style={styles.container}>
-      {userInfo ? <Text>{userInfo.name}</Text> : <Text>HomePage</Text>}
-    </View>
+    <ScrollView style={styles.container}>
+      {/*userInfo ? <Text>{userInfo.name}</Text> : <Text>HomePage</Text>*/}
+      <View style={styles.viewContainer}>{content}</View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
+    backgroundColor: "#e8e4e3",
+    marginTop: 30,
+  },
+  viewContainer: {
+    padding: 20,
   },
 });
 
